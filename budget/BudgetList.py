52f36a6be15d9ecc0fb44babcb74478feeb1e28e
Matthew Wright <mwright137@gmail.com>
2021-02-22 13:07:55 -0500

    module 4 fixed, really

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -30,7 +30,7 @@ class BudgetList():
   def __next__(self):
     try:
       return self.iter_e.__next__()
-    except:
+    except StopIteration as stop:      
       return self.iter_o.__next__()
 
 
